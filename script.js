542c3c9c52d4d005347493ef58f7ad91a42d375a
Lucas Pires <lucas.marcolino.093@ufrn.edu.br>
2023-12-09 18:27:58 -0300

    verificação de vitória

--- script.js
+++ script.js
@@ -35,5 +35,40 @@ function cellClick(row, col) {
         gameBoard[row][col] = currPlayer;
         currPlayer = currPlayer === 'X' ? 'O' : 'X';
     }
+    console.log(checkVictory(row,col));
     renderBoard();
+}
+
+function checkVictory(row, col){
+    // Verify main diagonal
+    if(row == col) return verifyMainDiagonal();
+    // Verify secondary diagonal
+    else if(row == boardSize - 1 - col) return verifySecondaryDiagonal();
+    // Verify for both rows and columns
+    return verifyRowAndCow(row, col);
+}
+
+function verifyMainDiagonal(){
+    let count = 0;
+    for(let i = 1; i < boardSize; ++i){
+        if(gameBoard[i-1][i-1] == gameBoard[i][i]) ++count;
+    }
+    return count == boardSize-1 ? 'Venceu' : 'Ainda não venceu';
+}
+
+function verifySecondaryDiagonal(){
+    let count = 0;
+    for(let i = 0; i < boardSize; ++i){
+        if(gameBoard[i][boardSize - 1 - i] == gameBoard[0][boardSize - 1]) ++count;
+    }
+    return count == boardSize ? 'Venceu' : 'Ainda não venceu';
+}
+
+function verifyRowAndCow(row, col){
+    let countRow = countCol = 0;
+    for(let i = 1; i < boardSize; ++i){
+        if(gameBoard[row][i-1] == gameBoard[row][i]) ++countRow;
+        if(gameBoard[i-1][col] == gameBoard[i][col]) ++countCol;
+    }
+    return (countRow == boardSize - 1) || (countCol == boardSize - 1) ? 'Venceu' : 'Ainda não venceu';
 }
